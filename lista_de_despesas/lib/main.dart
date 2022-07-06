566c51c7ad4bfde975fce63f3f3d551b1d281598
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2022-07-06 16:33:53 -0300

    Inicio do projeto criando o layout

---
+++ lista_de_despesas/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Lista de Despesas',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Despesas'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          Container(),
+        ],
+      ),
+    );
+  }
+}
